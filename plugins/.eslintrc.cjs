7508049642ad221bc0946932f483f130f5c9f721
Loren Segal <lsegal@soen.ca>
2024-11-20 12:13:41 -0800

    Enable eslint in qlty.toml (#1190)

---
+++ plugins/.eslintrc.cjs
@@ -0,0 +1,35 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  ignorePatterns: ["linters"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["unused-imports"],
+  extends: ["eslint:recommended"],
+  rules: {
+    "unused-imports/no-unused-imports": "warn",
+  },
+  overrides: [
+    {
+      files: ["**/*.{ts,tsx}"],
+      plugins: ["@typescript-eslint", "import"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "plugins/tsconfig.json",
+      },
+      extends: [
+        "plugin:@typescript-eslint/recommended",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+      ],
+      rules: {
+        "import/no-unresolved": "off",
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-floating-promises": "warn",
+        "@typescript-eslint/no-misused-promises": "warn",
+      },
+    },
+  ],
+};
