e993977c5121af6a684cde72c9ecea8d592820b2
Arslan <49312804+marschattha@users.noreply.github.com>
2024-11-28 05:25:01 +0500

    Fix trycmd tests (#1236)
    The trycmd tests haven't been running it seems since the workspace
    overhaul.

--- qlty-cli/tests/cmd.rs
+++ qlty-cli/tests/cmd.rs
@@ -3,77 +3,71 @@ use trycmd::TestCases;
 
 #[test]
 fn version_tests() {
-    TestCases::new().case("qlty/tests/cmd/version/**/*.toml");
+    TestCases::new().case("tests/cmd/version/**/*.toml");
 }
 
 #[test]
 fn help_tests() {
-    TestCases::new().case("qlty/tests/cmd/help/**/*.toml");
+    TestCases::new().case("tests/cmd/help/**/*.toml");
 }
 
 #[test]
 fn metrics_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/metrics/**/*.toml");
+    setup_and_run_test_cases("tests/cmd/metrics/**/*.toml");
 }
 
 #[test]
 fn duplication_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/duplication/**/*.toml");
+    setup_and_run_test_cases("tests/cmd/duplication/**/*.toml");
 }
 
 #[test]
 fn check_tests() {
     // only run .toml files in check directory
     // prevent running toml files from *.in
-    setup_and_run_test_cases("qlty/tests/cmd/check/*.toml");
+    setup_and_run_test_cases("tests/cmd/check/*.toml");
 }
 
 #[test]
 #[ignore] // ignore tests that may require network connection
-fn network_check_tests() {
-    // only run .toml files in check/network directory
-    // prevent running toml files from *.in
-    setup_and_run_test_cases("qlty/tests/cmd/check/network/*.toml");
-}
-
-#[test]
-#[ignore] // also may require network connection
-fn fmt_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/fmt/**/*.toml");
+fn network_tests() {
+    // only run .toml files in check/network/*/ directory
+    // Run check and fmt network in sequence
+    setup_and_run_test_cases("tests/cmd/check/network/*/*.toml");
 }
 
 #[test]
 fn smells_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/smells/**/*.toml");
+    setup_and_run_test_cases("tests/cmd/smells/**/*.toml");
 }
 
 #[test]
 fn coverage_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/coverage/**/*.toml");
+    setup_and_run_test_cases("tests/cmd/coverage/**/*.toml");
 }
 
 #[test]
 fn build_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/build/**/*.toml");
+    setup_and_run_test_cases("tests/cmd/build/**/*.toml");
 }
 
 #[test]
 fn init_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/init/*.toml");
+    setup_and_run_test_cases("tests/cmd/init/*.toml");
 }
 
 #[test]
 fn config_migrate_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/config/migrate/*.toml");
+    setup_and_run_test_cases("tests/cmd/config/migrate/*.toml");
 }
 
 #[test]
 #[ignore] // ignore tests that require network connection
 fn init_network_tests() {
-    setup_and_run_test_cases("qlty/tests/cmd/init/network/*.toml");
+    setup_and_run_test_cases("tests/cmd/init/network/*.toml");
 }
 
 #[test]
 fn git_based_check_tests() {
-    setup_and_run_diff_test_cases("qlty/tests/cmd/check/diff_tests/*.toml");
+    setup_and_run_diff_test_cases("tests/cmd/check/diff_tests/*.toml");
 }
--- qlty-cli/tests/lang.rs
+++ qlty-cli/tests/lang.rs
@@ -2,50 +2,50 @@ use crate::helpers::setup_and_run_test_cases;
 
 #[test]
 fn rust_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/rust/**/*.toml");
+    setup_and_run_test_cases("tests/lang/rust/**/*.toml");
 }
 
 #[test]
 fn kotlin_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/kotlin/**/*.toml");
+    setup_and_run_test_cases("tests/lang/kotlin/**/*.toml");
 }
 
 #[test]
 fn ruby_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/ruby/**/*.toml");
+    setup_and_run_test_cases("tests/lang/ruby/**/*.toml");
 }
 
 #[test]
 fn javascript_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/javascript/**/*.toml");
+    setup_and_run_test_cases("tests/lang/javascript/**/*.toml");
 }
 
 #[test]
 fn typescript_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/typescript/**/*.toml");
+    setup_and_run_test_cases("tests/lang/typescript/**/*.toml");
 }
 
 #[test]
 fn tsx_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/tsx/**/*.toml");
+    setup_and_run_test_cases("tests/lang/tsx/**/*.toml");
 }
 
 #[test]
 fn php_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/php/**/*.toml");
+    setup_and_run_test_cases("tests/lang/php/**/*.toml");
 }
 
 #[test]
 fn java_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/java/**/*.toml");
+    setup_and_run_test_cases("tests/lang/java/**/*.toml");
 }
 
 #[test]
 fn python_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/python/**/*.toml");
+    setup_and_run_test_cases("tests/lang/python/**/*.toml");
 }
 
 #[test]
 fn go_tests() {
-    setup_and_run_test_cases("qlty/tests/lang/go/**/*.toml");
+    setup_and_run_test_cases("tests/lang/go/**/*.toml");
 }
