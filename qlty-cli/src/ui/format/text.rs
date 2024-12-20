5bb54bdbe437c8660efcd867cc661933df311dc7
Bryan Helmkamp <19+brynary@users.noreply.github.com>
2024-12-19 22:16:51 -0500

    Move TextFormatter outputs into struct (#1345)
    This is an intentionally boring refactor which is the first of a series
    of refactors to cleanup TextFormatter before we add more logic to it

--- qlty-cli/src/ui/format/text.rs
+++ qlty-cli/src/ui/format/text.rs
@@ -56,50 +56,13 @@ impl fmt::Display for Line {
 impl Formatter for TextFormatter {
     fn write_to(&self, writer: &mut dyn std::io::Write) -> anyhow::Result<()> {
         if !self.summary {
-            print_unformatted(writer, &self.report)?;
+            self.print_unformatted(writer)?;
             self.print_fixes(writer)?;
-            print_issues(writer, &self.report)?;
+            self.print_issues(writer)?;
         }
 
-        print_invocations(writer, &self.report, self.verbose)?;
-
-        if self.verbose >= 1 && self.report.targets_count() > 0 {
-            writeln!(
-                writer,
-                "{} {} {}{}",
-                match self.report.verb {
-                    ExecutionVerb::Check => "Checked",
-                    ExecutionVerb::Fmt => "Formatted",
-                    _ => "Processed",
-                },
-                self.report.targets_count().to_formatted_string(&Locale::en),
-                if self.report.target_mode.is_diff() {
-                    "modified "
-                } else {
-                    ""
-                },
-                if self.report.targets_count() == 1 {
-                    "file"
-                } else {
-                    "files"
-                },
-            )?;
-        } else if self.report.targets_count() == 0 && self.report.target_mode.is_diff() {
-            writeln!(
-                writer,
-                "{}",
-                style(format!(
-                    "No modified files for {} were found on your branch.",
-                    if self.report.verb == ExecutionVerb::Fmt {
-                        "formatting"
-                    } else {
-                        "checks"
-                    }
-                ))
-                .dim()
-            )?;
-        }
-
+        self.print_invocations(writer)?;
+        self.print_conclusion(writer)?;
         Ok(())
     }
 }
@@ -120,6 +83,227 @@ enum AskMode {
 }
 
 impl TextFormatter {
+    pub fn print_unformatted(&self, writer: &mut dyn std::io::Write) -> Result<()> {
+        let issues = self
+            .report
+            .issues
+            .iter()
+            .filter(|issue| issue.level() == Level::Fmt)
+            .collect::<Vec<_>>();
+
+        let paths = issues
+            .iter()
+            .map(|issue| issue.path().clone())
+            .collect::<HashSet<_>>();
+
+        let mut paths: Vec<_> = paths.iter().collect();
+        paths.sort();
+
+        if !paths.is_empty() {
+            writeln!(writer)?;
+            writeln!(
+                writer,
+                "{}{}{}",
+                style(" UNFORMATTED FILES: ").bold().reverse(),
+                style(paths.len().to_formatted_string(&Locale::en))
+                    .bold()
+                    .reverse(),
+                style(" ").bold().reverse()
+            )?;
+            writeln!(writer)?;
+        }
+
+        for path in paths {
+            writeln!(
+                writer,
+                "{} {}",
+                style("✖").red().bold(),
+                style(path_to_string(path.clone().unwrap_or_default())).underlined(),
+            )?;
+        }
+
+        Ok(())
+    }
+
+    pub fn print_issues(&self, writer: &mut dyn std::io::Write) -> Result<()> {
+        let issues_by_path = self.report.issues_by_path();
+        let mut paths: Vec<_> = issues_by_path.keys().collect();
+        paths.sort();
+
+        if !paths.is_empty() {
+            writeln!(writer)?;
+            writeln!(
+                writer,
+                "{}{}{}",
+                style(" ISSUES: ").bold().reverse(),
+                style(self.report.issues.len().to_formatted_string(&Locale::en))
+                    .bold()
+                    .reverse(),
+                style(" ").bold().reverse()
+            )?;
+            writeln!(writer)?;
+        }
+
+        for path in paths {
+            let issues = issues_by_path.get(path).unwrap();
+
+            let first_issue = issues.first().unwrap();
+            let start_line = first_issue.range().unwrap_or_default().start_line;
+            let end_line = first_issue.range().unwrap_or_default().end_line;
+
+            writeln!(
+                writer,
+                "{}{}",
+                style(path_to_string(path.clone().unwrap_or_default())).underlined(),
+                style(format!(":{}:{}", start_line, end_line)).dim()
+            )?;
+
+            let mut tw = TabWriter::new(vec![]);
+
+            for issue in issues {
+                tw.write_all(
+                    format!(
+                        "{:>7}\t{}\t{}\t{}{}\n",
+                        style(format!(
+                            "{}:{}",
+                            issue.range().unwrap_or_default().start_line,
+                            issue.range().unwrap_or_default().end_line,
+                        ))
+                        .dim(),
+                        formatted_level(issue.level()),
+                        issue.message.replace('\n', " ").trim(),
+                        formatted_source(issue),
+                        formatted_fix_message(&self.report, issue),
+                    )
+                    .as_bytes(),
+                )
+                .unwrap();
+            }
+
+            tw.flush().unwrap();
+            let written = String::from_utf8(tw.into_inner().unwrap()).unwrap();
+            writeln!(writer, "{}", written)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn print_invocations(&self, writer: &mut dyn std::io::Write) -> Result<()> {
+        for formatted_path in &self.report.formatted {
+            writeln!(
+                writer,
+                "{} Formatted {}",
+                style("✔").green().bold(),
+                style(path_to_string(formatted_path)).underlined()
+            )?;
+        }
+
+        if self.verbose >= 1 {
+            writeln!(writer)?;
+            writeln!(
+                writer,
+                "{}{}{}",
+                style(" JOBS: ").bold().reverse(),
+                style(
+                    self.report
+                        .invocations
+                        .len()
+                        .to_formatted_string(&Locale::en)
+                )
+                .bold()
+                .reverse(),
+                style(" ").bold().reverse()
+            )?;
+            writeln!(writer)?;
+        }
+
+        let mut printed_summary = false;
+        let cwd = std::env::current_dir().expect("Unable to identify current directory");
+
+        for invocation in &self.report.invocations {
+            let absolute_outfile_path = invocation.outfile_path();
+            let outfile_path = pathdiff::diff_paths(absolute_outfile_path, &cwd).unwrap();
+
+            match invocation.status() {
+                InvocationStatus::Success => {
+                    if self.verbose >= 1 {
+                        writeln!(
+                            writer,
+                            "{} {} checked {} files in {:.2}s {}",
+                            style("Success").green(),
+                            invocation.invocation.plugin_name,
+                            invocation.plan.workspace_entries.len(),
+                            invocation.invocation.duration_secs,
+                            style(path_to_string(outfile_path)).dim(),
+                        )?;
+
+                        printed_summary = true;
+                    }
+                }
+                InvocationStatus::LintError => match invocation.invocation.exit_code {
+                    Some(code) => {
+                        writeln!(
+                            writer,
+                            "{} {}: Exited with code {:?} {}",
+                            style("Lint error").red(),
+                            style(&invocation.invocation.plugin_name).red().bold(),
+                            code,
+                            style(path_to_string(outfile_path)).dim(),
+                        )?;
+
+                        if invocation.invocation.stderr.is_empty() {
+                            if !invocation.invocation.stdout.is_empty() {
+                                let text: String =
+                                    invocation.invocation.stdout.chars().take(2048).collect();
+
+                                for line in text.lines() {
+                                    writeln!(writer, "        {}", style(line).red())?;
+                                }
+                            }
+                        } else {
+                            let text: String =
+                                invocation.invocation.stderr.chars().take(2048).collect();
+
+                            for line in text.lines() {
+                                writeln!(writer, "        {}", style(line).red())?;
+                            }
+                        }
+
+                        printed_summary = true;
+                    }
+                    None => {
+                        writeln!(
+                            writer,
+                            "{} {}: Exited with unknown status {}",
+                            style("Lint error").red(),
+                            style(&invocation.invocation.plugin_name).red().bold(),
+                            style(path_to_string(invocation.outfile_path())).dim(),
+                        )?;
+                        printed_summary = true;
+                    }
+                },
+                InvocationStatus::ParseError => {
+                    writeln!(
+                        writer,
+                        "{} {}: {} {}",
+                        style("Parse error").red(),
+                        invocation.invocation.plugin_name,
+                        invocation.invocation.parser_error.as_ref().unwrap(),
+                        style(path_to_string(outfile_path)).dim(),
+                    )?;
+
+                    printed_summary = true;
+                }
+            }
+        }
+
+        if printed_summary {
+            writeln!(writer)?;
+        }
+
+        Ok(())
+    }
+
     pub fn print_fixes(&self, writer: &mut dyn std::io::Write) -> Result<()> {
         let mut patch_candidates = vec![];
 
@@ -285,6 +469,59 @@ impl TextFormatter {
 
         Ok(())
     }
+
+    pub fn print_conclusion(&self, writer: &mut dyn std::io::Write) -> Result<()> {
+        if self.verbose >= 1 && self.report.targets_count() > 0 {
+            self.print_processed_files(writer)?;
+        } else if self.report.targets_count() == 0 && self.report.target_mode.is_diff() {
+            self.print_no_modified_files(writer)?;
+        }
+
+        Ok(())
+    }
+
+    pub fn print_processed_files(&self, writer: &mut dyn std::io::Write) -> Result<()> {
+        writeln!(
+            writer,
+            "{} {} {}{}",
+            match self.report.verb {
+                ExecutionVerb::Check => "Checked",
+                ExecutionVerb::Fmt => "Formatted",
+                _ => "Processed",
+            },
+            self.report.targets_count().to_formatted_string(&Locale::en),
+            if self.report.target_mode.is_diff() {
+                "modified "
+            } else {
+                ""
+            },
+            if self.report.targets_count() == 1 {
+                "file"
+            } else {
+                "files"
+            },
+        )?;
+
+        Ok(())
+    }
+
+    pub fn print_no_modified_files(&self, writer: &mut dyn std::io::Write) -> Result<()> {
+        writeln!(
+            writer,
+            "{}",
+            style(format!(
+                "No modified files for {} were found on your branch.",
+                if self.report.verb == ExecutionVerb::Fmt {
+                    "formatting"
+                } else {
+                    "checks"
+                }
+            ))
+            .dim()
+        )?;
+
+        Ok(())
+    }
 }
 
 fn prompt_apply_this_fix() -> Result<String> {
@@ -324,225 +561,6 @@ fn apply_fix(writer: &mut dyn std::io::Write, candidate: &PatchCandidate) -> Res
             style("Failed to parse patch:").red(),
             style(&candidate.path).underlined()
         )?;
-    }
-
-    Ok(())
-}
-
-pub fn print_unformatted(writer: &mut dyn std::io::Write, report: &Report) -> Result<()> {
-    let issues = report
-        .issues
-        .iter()
-        .filter(|issue| issue.level() == Level::Fmt)
-        .collect::<Vec<_>>();
-
-    let paths = issues
-        .iter()
-        .map(|issue| issue.path().clone())
-        .collect::<HashSet<_>>();
-
-    let mut paths: Vec<_> = paths.iter().collect();
-    paths.sort();
-
-    if !paths.is_empty() {
-        writeln!(writer)?;
-        writeln!(
-            writer,
-            "{}{}{}",
-            style(" UNFORMATTED FILES: ").bold().reverse(),
-            style(paths.len().to_formatted_string(&Locale::en))
-                .bold()
-                .reverse(),
-            style(" ").bold().reverse()
-        )?;
-        writeln!(writer)?;
-    }
-
-    for path in paths {
-        writeln!(
-            writer,
-            "{} {}",
-            style("✖").red().bold(),
-            style(path_to_string(path.clone().unwrap_or_default())).underlined(),
-        )?;
-    }
-
-    Ok(())
-}
-
-pub fn print_issues(writer: &mut dyn std::io::Write, report: &Report) -> Result<()> {
-    let issues_by_path = report.issues_by_path();
-    let mut paths: Vec<_> = issues_by_path.keys().collect();
-    paths.sort();
-
-    if !paths.is_empty() {
-        writeln!(writer)?;
-        writeln!(
-            writer,
-            "{}{}{}",
-            style(" ISSUES: ").bold().reverse(),
-            style(report.issues.len().to_formatted_string(&Locale::en))
-                .bold()
-                .reverse(),
-            style(" ").bold().reverse()
-        )?;
-        writeln!(writer)?;
-    }
-
-    for path in paths {
-        let issues = issues_by_path.get(path).unwrap();
-
-        let first_issue = issues.first().unwrap();
-        let start_line = first_issue.range().unwrap_or_default().start_line;
-        let end_line = first_issue.range().unwrap_or_default().end_line;
-
-        writeln!(
-            writer,
-            "{}{}",
-            style(path_to_string(path.clone().unwrap_or_default())).underlined(),
-            style(format!(":{}:{}", start_line, end_line)).dim()
-        )?;
-
-        let mut tw = TabWriter::new(vec![]);
-
-        for issue in issues {
-            tw.write_all(
-                format!(
-                    "{:>7}\t{}\t{}\t{}{}\n",
-                    style(format!(
-                        "{}:{}",
-                        issue.range().unwrap_or_default().start_line,
-                        issue.range().unwrap_or_default().end_line,
-                    ))
-                    .dim(),
-                    formatted_level(issue.level()),
-                    issue.message.replace('\n', " ").trim(),
-                    formatted_source(issue),
-                    formatted_fix_message(report, issue),
-                )
-                .as_bytes(),
-            )
-            .unwrap();
-        }
-
-        tw.flush().unwrap();
-        let written = String::from_utf8(tw.into_inner().unwrap()).unwrap();
-        writeln!(writer, "{}", written)?;
-    }
-
-    Ok(())
-}
-
-pub fn print_invocations(
-    writer: &mut dyn std::io::Write,
-    report: &Report,
-    verbose: usize,
-) -> Result<()> {
-    for formatted_path in &report.formatted {
-        writeln!(
-            writer,
-            "{} Formatted {}",
-            style("✔").green().bold(),
-            style(path_to_string(formatted_path)).underlined()
-        )?;
-    }
-
-    if verbose >= 1 {
-        writeln!(writer)?;
-        writeln!(
-            writer,
-            "{}{}{}",
-            style(" JOBS: ").bold().reverse(),
-            style(report.invocations.len().to_formatted_string(&Locale::en))
-                .bold()
-                .reverse(),
-            style(" ").bold().reverse()
-        )?;
-        writeln!(writer)?;
-    }
-
-    let mut printed_summary = false;
-    let cwd = std::env::current_dir().expect("Unable to identify current directory");
-
-    for invocation in &report.invocations {
-        let absolute_outfile_path = invocation.outfile_path();
-        let outfile_path = pathdiff::diff_paths(absolute_outfile_path, &cwd).unwrap();
-
-        match invocation.status() {
-            InvocationStatus::Success => {
-                if verbose >= 1 {
-                    writeln!(
-                        writer,
-                        "{} {} checked {} files in {:.2}s {}",
-                        style("Success").green(),
-                        invocation.invocation.plugin_name,
-                        invocation.plan.workspace_entries.len(),
-                        invocation.invocation.duration_secs,
-                        style(path_to_string(outfile_path)).dim(),
-                    )?;
-
-                    printed_summary = true;
-                }
-            }
-            InvocationStatus::LintError => match invocation.invocation.exit_code {
-                Some(code) => {
-                    writeln!(
-                        writer,
-                        "{} {}: Exited with code {:?} {}",
-                        style("Lint error").red(),
-                        style(&invocation.invocation.plugin_name).red().bold(),
-                        code,
-                        style(path_to_string(outfile_path)).dim(),
-                    )?;
-
-                    if invocation.invocation.stderr.is_empty() {
-                        if !invocation.invocation.stdout.is_empty() {
-                            let text: String =
-                                invocation.invocation.stdout.chars().take(2048).collect();
-
-                            for line in text.lines() {
-                                writeln!(writer, "        {}", style(line).red())?;
-                            }
-                        }
-                    } else {
-                        let text: String =
-                            invocation.invocation.stderr.chars().take(2048).collect();
-
-                        for line in text.lines() {
-                            writeln!(writer, "        {}", style(line).red())?;
-                        }
-                    }
-
-                    printed_summary = true;
-                }
-                None => {
-                    writeln!(
-                        writer,
-                        "{} {}: Exited with unknown status {}",
-                        style("Lint error").red(),
-                        style(&invocation.invocation.plugin_name).red().bold(),
-                        style(path_to_string(invocation.outfile_path())).dim(),
-                    )?;
-                    printed_summary = true;
-                }
-            },
-            InvocationStatus::ParseError => {
-                writeln!(
-                    writer,
-                    "{} {}: {} {}",
-                    style("Parse error").red(),
-                    invocation.invocation.plugin_name,
-                    invocation.invocation.parser_error.as_ref().unwrap(),
-                    style(path_to_string(outfile_path)).dim(),
-                )?;
-
-                printed_summary = true;
-            }
-        }
-    }
-
-    if printed_summary {
-        writeln!(writer)?;
     }
 
     Ok(())
