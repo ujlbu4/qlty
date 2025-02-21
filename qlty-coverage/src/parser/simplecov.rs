ff0eb52ad479a9934049d9badd80680d92b6b2bf
Noah Davis <11672+noahd1@users.noreply.github.com>
2025-02-21 05:29:20 -0800

    Support SimpleCov "merged" Coverage format (#1493)

--- qlty-coverage/src/parser/simplecov.rs
+++ qlty-coverage/src/parser/simplecov.rs
@@ -18,74 +18,31 @@ impl Parser for Simplecov {
         let json_value: Value =
             serde_json::from_str(text).with_context(|| "Failed to parse JSON text")?;
 
-        let mut file_coverages = vec![];
-        let mut optional_coverage_data = Map::new();
+        let formatters: Vec<Box<dyn SimplecovFormatter>> = vec![
+            Box::new(Simplecov018OrNewer {}),
+            Box::new(SimplecovLegacy {}),
+            Box::new(SimplecovJson {}),
+        ];
 
-        let coverage_data = if self.is_version_018_or_newer(&json_value) {
-            json_value.get("coverage").and_then(|c| c.as_object())
-        } else {
-            let coverage = json_value
-                .as_object()
-                .and_then(|obj| obj.values().next())
-                .and_then(|group| group.get("coverage").and_then(|c| c.as_object()));
-            if coverage.is_some() {
-                coverage.to_owned()
-            } else {
-                // Check if using simplecov-json[https://github.com/vicentllongo/simplecov-json]
-                Self::extract_coverage_data_for_simplecov_json(
-                    &json_value,
-                    &mut optional_coverage_data,
-                )
-            }
-        };
-
-        if let Some(coverage) = coverage_data {
-            for (file_path, data) in coverage {
-                let line_hits = self.parse_line_coverage(data);
-
-                let file_coverage = FileCoverage {
-                    path: file_path.to_string(),
-                    hits: line_hits,
-                    ..Default::default()
-                };
-
-                file_coverages.push(file_coverage);
+        for formatter in formatters {
+            if formatter.matches(&json_value) {
+                return formatter.parse(&json_value);
             }
         }
 
-        Ok(file_coverages)
+        Ok(vec![])
     }
 }
 
-impl Simplecov {
-    fn parse_line_coverage(&self, data: &Value) -> Vec<i64> {
-        match data {
-            Value::Object(obj) => {
-                // Post-0.18.0 format with "lines" key
-                obj.get("lines")
-                    .and_then(|v| v.as_array())
-                    .map_or(vec![], |arr| {
-                        arr.iter().map(|x| self.parse_lines(x)).collect()
-                    })
-            }
-            Value::Array(arr) => {
-                // Pre-0.18.0 format, directly an array
-                arr.iter().map(|x| self.parse_lines(x)).collect()
-            }
-            _ => vec![],
-        }
-    }
+trait SimplecovFormatter {
+    fn matches(&self, json_value: &Value) -> bool;
+    fn parse(&self, json_value: &Value) -> Result<Vec<FileCoverage>>;
+}
 
-    fn parse_lines(&self, value: &Value) -> i64 {
-        match value {
-            Value::Number(n) => n.as_i64().unwrap_or(-1),
-            Value::String(s) if s == "ignored" => -2,
-            Value::Null => -1,
-            _ => -1,
-        }
-    }
+struct Simplecov018OrNewer;
 
-    fn is_version_018_or_newer(&self, json_value: &serde_json::Value) -> bool {
+impl SimplecovFormatter for Simplecov018OrNewer {
+    fn matches(&self, json_value: &Value) -> bool {
         if let Some(meta) = json_value.get("meta") {
             if let Some(version_str) = meta.get("simplecov_version").and_then(|v| v.as_str()) {
                 if let Ok(version) = Version::parse(version_str) {
@@ -96,11 +53,48 @@ impl Simplecov {
         false
     }
 
-    fn extract_coverage_data_for_simplecov_json<'a>(
-        json_value: &'a serde_json::Value,
-        coverage_data: &'a mut Map<String, Value>,
-    ) -> Option<&'a Map<String, Value>> {
-        // Check if using simplecov-json[https://github.com/vicentllongo/simplecov-json]
+    fn parse(&self, json_value: &Value) -> Result<Vec<FileCoverage>> {
+        let mut file_coverages = vec![];
+        if let Some(coverage) = json_value.get("coverage").and_then(|c| c.as_object()) {
+            file_coverages.extend(Simplecov::extract_file_coverage(coverage));
+        }
+        Ok(file_coverages)
+    }
+}
+
+struct SimplecovLegacy;
+
+impl SimplecovFormatter for SimplecovLegacy {
+    fn matches(&self, json_value: &Value) -> bool {
+        if let Some(obj) = json_value.as_object() {
+            obj.values().any(|obj| obj.get("coverage").is_some())
+        } else {
+            false
+        }
+    }
+
+    fn parse(&self, json_value: &Value) -> Result<Vec<FileCoverage>> {
+        let mut file_coverages = vec![];
+        if let Some(groups) = json_value.as_object() {
+            for group in groups.values() {
+                if let Some(coverage) = group.get("coverage").and_then(|c| c.as_object()) {
+                    file_coverages.extend(Simplecov::extract_file_coverage(coverage));
+                }
+            }
+        }
+        Ok(file_coverages)
+    }
+}
+
+struct SimplecovJson;
+
+impl SimplecovFormatter for SimplecovJson {
+    fn matches(&self, json_value: &Value) -> bool {
+        json_value.get("files").is_some()
+    }
+
+    fn parse(&self, json_value: &Value) -> Result<Vec<FileCoverage>> {
+        let mut file_coverages = vec![];
         if let Some(files) = json_value.get("files").and_then(|v| v.as_array()) {
             for file in files {
                 if let (Some(filename), Some(coverage)) =
@@ -109,14 +103,53 @@ impl Simplecov {
                     if let (Some(filename_str), Some(coverage_arr)) =
                         (filename.as_str(), coverage.as_array())
                     {
-                        coverage_data
-                            .insert(filename_str.to_string(), Value::Array(coverage_arr.clone()));
+                        let line_hits =
+                            Simplecov::parse_line_coverage(&Value::Array(coverage_arr.clone()));
+                        file_coverages.push(FileCoverage {
+                            path: filename_str.to_string(),
+                            hits: line_hits,
+                            ..Default::default()
+                        });
                     }
                 }
             }
-            Some(coverage_data)
-        } else {
-            None
+        }
+        Ok(file_coverages)
+    }
+}
+
+impl Simplecov {
+    fn extract_file_coverage(map: &Map<String, Value>) -> Vec<FileCoverage> {
+        map.iter()
+            .map(|(key, value)| {
+                let line_hits = Self::parse_line_coverage(value);
+
+                FileCoverage {
+                    path: key.to_string(),
+                    hits: line_hits,
+                    ..Default::default()
+                }
+            })
+            .collect()
+    }
+
+    fn parse_line_coverage(data: &Value) -> Vec<i64> {
+        match data {
+            Value::Object(obj) => obj
+                .get("lines")
+                .and_then(|v| v.as_array())
+                .map_or(vec![], |arr| arr.iter().map(Self::parse_lines).collect()),
+            Value::Array(arr) => arr.iter().map(Self::parse_lines).collect(),
+            _ => vec![],
+        }
+    }
+
+    fn parse_lines(value: &Value) -> i64 {
+        match value {
+            Value::Number(n) => n.as_i64().unwrap_or(-1),
+            Value::String(s) if s == "ignored" => -2,
+            Value::Null => -1,
+            _ => -1,
         }
     }
 }
@@ -207,6 +240,41 @@ mod test {
             - "1"
             - "-1"
             - "-1"
+            - "-1"
+            - "-1"
+        "#);
+    }
+
+    #[test]
+    fn simplecov_legacy_report_two_sections() {
+        let input = r#"
+        {
+            "Unit Tests": {
+                "coverage": {
+                    "development/mygem/lib/mygem/errors.rb": [1, 0, 1, null]
+                },
+                "timestamp": 1488827968
+            },
+            "Integration Tests": {
+                "coverage": {
+                    "development/mygem/lib/mygem/errors.rb": [1, 2, null, null]
+                },
+                "timestamp": 1488827968
+            }
+        }
+        "#;
+        let results: Vec<FileCoverage> = Simplecov::new().parse_text(input).unwrap();
+        insta::assert_yaml_snapshot!(results, @r#"
+        - path: development/mygem/lib/mygem/errors.rb
+          hits:
+            - "1"
+            - "0"
+            - "1"
+            - "-1"
+        - path: development/mygem/lib/mygem/errors.rb
+          hits:
+            - "1"
+            - "2"
             - "-1"
             - "-1"
         "#);
