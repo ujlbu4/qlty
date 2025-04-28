b0e91ffd63945252e1248e748a0c74fe7e87e63c
Arslan <49312804+marschattha@users.noreply.github.com>
2025-04-28 21:28:33 +0500

    Fix: Include plugin configs into built-in compiled source (#1983)
    https://github.com/qltysh/cloud/issues/6019

--- qlty-plugins/src/lib.rs
+++ qlty-plugins/src/lib.rs
@@ -2,10 +2,11 @@ use rust_embed::Embed;
 
 #[derive(Embed)]
 #[folder = "plugins/"]
-#[include = "*.toml"]
-#[exclude = ".qlty/qlty.toml"]
-#[exclude = "*/fixtures/*"]
-#[exclude = "node_modules/*"]
+#[include = "linters/**"]
+#[exclude = "**/fixtures/**"]
+#[exclude = "**/README.md"]
+#[exclude = "**/*.test.ts"]
+
 pub struct Plugins;
 
 #[cfg(test)]
@@ -14,13 +15,43 @@ mod test {
 
     #[test]
     fn get_definitions() {
-        let eslint = Plugins::get("linters/eslint/plugin.toml").unwrap();
-        assert!(eslint.data.len() > 0);
+        let shellcheck = Plugins::get("linters/shellcheck/plugin.toml").unwrap();
+        assert!(shellcheck.data.len() > 0);
+    }
+
+    #[test]
+    fn get_plugin_config() {
+        let shellcheck_config = Plugins::get("linters/shellcheck/.shellcheckrc").unwrap();
+        assert!(shellcheck_config.data.len() > 0);
     }
 
     #[test]
     fn ignore_node_modules() {
         let has_node_modules = Plugins::iter().any(|path| path.contains("node_modules"));
         assert!(has_node_modules == false);
+    }
+
+    #[test]
+    fn ignore_fixtures() {
+        let has_fixtures = Plugins::iter().any(|path| path.contains("fixtures"));
+        assert!(has_fixtures == false);
+    }
+
+    #[test]
+    fn ignore_readme() {
+        let has_readme = Plugins::iter().any(|path| path.contains("README.md"));
+        assert!(has_readme == false);
+    }
+
+    #[test]
+    fn ignore_test_files() {
+        let has_test_files = Plugins::iter().any(|path| path.contains(".test.ts"));
+        assert!(has_test_files == false);
+    }
+
+    #[test]
+    fn ignore_unrelated_files() {
+        let has_package_json = Plugins::iter().any(|path| path.contains("package.json"));
+        assert!(has_package_json == false);
     }
 }
